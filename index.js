2c2fedfca0a28c82a3b84e1f90d028567d22f9eb
Angel Aguirre <angelaguirre.personal@gmail.com>
2022-08-18 20:39:17 -0500

    temp directory changed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload({
     useTempFiles: true,
-    tempFileDir: './uploads/temp/',
+    tempFileDir: '/temp/',
     createParentPath: true
 }))
 require('./utils/auth');
